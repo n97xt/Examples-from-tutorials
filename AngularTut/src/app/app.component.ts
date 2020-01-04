1be7a961fa0e244c3bac52681f6e4e5e95cff20a
N97XT <szymonbockowski97@gmail.com>
2020-01-04 20:20:17 +0100

    Subjects: Modules - LifeCycle hooks

--- AngularTut/src/app/app.component.ts
+++ AngularTut/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'AngularTut';
+  wyswietl = false;
+  osoby = [];
+
+  ngOnInit(){
+    console.log('komponent init');
+    this.osoby = [
+      {imie: 'Krystian', wiek: 21},
+      {imie: 'Damian', wiek: 34},
+      {imie: 'Romek', wiek: 32},
+      {imie: 'Darek', wiek: 41},
+    ]
+  }
+  ngOnDestroy() {
+    console.log('komponent zniszczony');
+  }
+
+  wyslij = function() {
+    this.wyswietl = !this.wyswietl;
+  }
 }
