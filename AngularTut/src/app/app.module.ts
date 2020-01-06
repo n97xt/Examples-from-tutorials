7a8149abd2a33f56f3d9b38af6e2de5aa7be66be
N97XT <szymonbockowski97@gmail.com>
2020-01-06 20:52:04 +0100

    Forms & IO communication beetwen components

--- AngularTut/src/app/app.module.ts
+++ AngularTut/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { OsobyService } from './osoby.service';
 import { AppRoutingModule } from './app-routing.module';
 import { JedenComponent } from './jeden/jeden.component';
 import { DwaComponent } from './dwa/dwa.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JedenComponent,
-    DwaComponent
+    DwaComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [OsobyService],
   bootstrap: [AppComponent]
--- AngularTut/src/app/jeden/jeden.component.ts
+++ AngularTut/src/app/jeden/jeden.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-jeden',
   templateUrl: './jeden.component.html',
   styleUrls: ['./jeden.component.css']
 })
 export class JedenComponent implements OnInit {
+
+  naszForm = new FormGroup({
+    imie: new FormControl(''),
+    nazwisko: new FormControl('')
+  });
+
+  klik: Boolean = false;
+
+  wyslij() {
+    console.log(this.naszForm.value);
+  }
+
+  klikniety(noweDane: boolean) {
+    this.klik = noweDane;
+  }
 
   constructor() { }
 
