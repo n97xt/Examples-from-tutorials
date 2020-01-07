0fceb47a19ceff8146f54d169c4f05cca6da709c
N97XT <szymonbockowski97@gmail.com>
2020-01-07 18:10:21 +0100

    Two initial component and one service

--- Filmy/src/app/app.module.ts
+++ Filmy/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaComponent } from './lista/lista.component';
+import { DetaleComponent } from './detale/detale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    DetaleComponent
   ],
   imports: [
     BrowserModule,
---
+++ Filmy/src/app/detale/detale.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detale',
+  templateUrl: './detale.component.html',
+  styleUrls: ['./detale.component.css']
+})
+export class DetaleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
