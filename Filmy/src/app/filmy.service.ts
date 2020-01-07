3cbaadd3be47d55e0e48e85f39be45d038a53662
N97XT <szymonbockowski97@gmail.com>
2020-01-07 18:34:58 +0100

    Add data about movies to array filmy

--- Filmy/src/app/filmy.service.ts
+++ Filmy/src/app/filmy.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { Film } from './modele/film';
 @Injectable({
   providedIn: 'root'
 })
 export class FilmyService {
 
-  filmy: Film[] = [{id: 0, tytul: '123', rok: 2005}];
+  private filmy: Film[] = [
+    {id: 0, tytul: 'Titatnic', opis: 'Rok 1912, brytyjski statek Titanic wyrusza w swój dziewiczy rejs do USA. Na pokładzie emigrant Jack przypadkowo spotyka arystokratkę Rose.', rok: 1997},
+    {id: 1, tytul: 'Terminator', opis: 'Elektroniczny morderca zostaje wysłany z przyszłości do roku 1984, by zabić matkę przyszłego lidera rewolucji. W ślad za nim rusza Kyle Reese, który ma chronić kobietę.', rok: 1984},
+    {id: 2, tytul: 'Avatar', opis: 'Jake, sparaliżowany były komandos, otrzymuje misję i zostaje wysłany na planetę Pandora, gdzie zaprzyjaźnia się z lokalną społecznością oraz postanawia jej pomóc.', rok: 2009},
+  ];
 
   constructor() { }
 }
