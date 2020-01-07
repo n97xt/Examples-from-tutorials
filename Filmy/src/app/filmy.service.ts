81e4fb981002cbba314f4a0702b1f19f362c9253
N97XT <szymonbockowski97@gmail.com>
2020-01-07 18:17:32 +0100

    New own type of array and new empty array Film

--- Filmy/src/app/filmy.service.ts
+++ Filmy/src/app/filmy.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
+interface Film {
+  tytul: String;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FilmyService {
+
+  filmy: Film[] = [];
 
   constructor() { }
 }
