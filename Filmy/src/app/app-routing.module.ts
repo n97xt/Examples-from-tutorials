e87bd1d5fa5b10cf6eec353a0732842df4a64a17
N97XT <szymonbockowski97@gmail.com>
2020-01-08 23:13:54 +0100

    Routing for movies details

--- Filmy/src/app/app-routing.module.ts
+++ Filmy/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaComponent } from './lista/lista.component';
+import { DetaleComponent } from './detale/detale.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListaComponent},
+  {path: 'film/:id', component: DetaleComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
