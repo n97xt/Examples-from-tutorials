488af6be7a35dd2aa1166d1d8f4fc455be3df73a
N97XT <szymonbockowski97@gmail.com>
2020-01-08 23:16:37 +0100

    Routing for movies details

--- Filmy/src/app/detale/detale.component.ts
+++ Filmy/src/app/detale/detale.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detale',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetaleComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log('id', id);
   }
 
 }
