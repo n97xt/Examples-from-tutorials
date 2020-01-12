b1c85711e9a2d9881cc2106cfcae099011ca0c76
N97XT <szymonbockowski97@gmail.com>
2020-01-12 23:14:14 +0100

    Add function to write csv file

--- projekt-python/venv/start.py
+++ projekt-python/venv/start.py
@@ -7,15 +7,25 @@ def wczytaj_osoby():
         plik = open('osoby.csv', 'r')
         csv_reader = csv.reader(plik)
         for row in csv_reader:
-            osoba = {'id': int(row[0]), 'imie':row[1], 'pass':row[2]}
+            osoba = {'id': row[0], 'imie':row[1], 'pass':row[2]}
             osoby.append(osoba)
     except FileNotFoundError:
         plik = open('osoby.csv', 'w')
         plik.close()
+
+def dodaj_osobe():
+    imie = input("Podaj imie: ")
+    plik = open('osoby.csv', 'a', newline='')
+    csv_writer = csv.writer(plik, delimiter=",")
+    csv_writer.writerow([0, imie, "asdasda"])
 
 wczytaj_osoby()
 
 czy_lista = input("Czy chcesz wyświetlić liste? (T/N) ")
 if czy_lista == "T" or czy_lista == "t":
     print(osoby)
+
+czy_dodac = input("Czy chcesz dodać nową osobę? (T/N) ")
+if czy_dodac == "T" or czy_lista == "t":
+    dodaj_osobe()
 
