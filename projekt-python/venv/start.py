edd13632938e767c3de08c3f0cc8e8527d39b31d
N97XT <szymonbockowski97@gmail.com>
2020-01-12 23:02:38 +0100

    Add function to read csv file

--- projekt-python/venv/start.py
+++ projekt-python/venv/start.py
@@ -1,6 +1,21 @@
+import csv
+
 osoby = []
+
+def wczytaj_osoby():
+    try:
+        plik = open('osoby.csv', 'r')
+        csv_reader = csv.reader(plik)
+        for row in csv_reader:
+            osoba = {'id': int(row[0]), 'imie':row[1], 'pass':row[2]}
+            osoby.append(osoba)
+    except FileNotFoundError:
+        plik = open('osoby.csv', 'w')
+        plik.close()
+
+wczytaj_osoby()
 
 czy_lista = input("Czy chcesz wyświetlić liste? (T/N) ")
 if czy_lista == "T" or czy_lista == "t":
-    print("osoby")
+    print(osoby)
 
