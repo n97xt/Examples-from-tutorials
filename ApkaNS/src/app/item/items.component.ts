f1e3d15e89c6767c25210829c38ab4f737aac4bc
N97XT <szymonbockowski97@gmail.com>
2020-01-27 22:10:38 +0100

    Finish the basic NS tutorial

--- ApkaNS/src/app/item/items.component.ts
+++ ApkaNS/src/app/item/items.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from "@angular/core";
-
+import { EventData } from "tns-core-modules/data/observable";
+import { Page } from "tns-core-modules/ui/page";
 import { Item } from "./item";
 import { ItemService } from "./item.service";
 
 @Component({
     selector: "ns-items",
-    templateUrl: "./items.component.html"
+    templateUrl: "./items.component.html",
+    styleUrls: ["./items.component.css"]
 })
-export class ItemsComponent implements OnInit {
-    items: Array<Item>;
 
-    constructor(private itemService: ItemService) { }
+export class ItemsComponent implements OnInit {
+    imie: String;
+
+    constructor(private itemService: ItemService, page: Page) { 
+        page.actionBarHidden = true;
+    } 
 
     ngOnInit(): void {
-        this.items = this.itemService.getItems();
+        this.imie = "Szymon";
+    }
+
+    onTap(event) {
+        this.imie = "Damian";
     }
 }
+
+
